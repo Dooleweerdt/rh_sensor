cb627af78cec1060e4e2d8a1d9ca2c9b56521fff
Brian Rasmussen <brian.rasmussen80@gmail.com>
2026-05-10 21:51:26 +0200

    Added trait implementation of onewire and sht3x sensors. Needs rework on their value readout, as it is fixed now...

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -8,14 +8,14 @@
 // build.  So, this is just always necessary.
 #![allow(unexpected_cfgs)]
 
-mod driver;
+mod rht_sensor;
 mod rht_sensor_init;
 
 use log::info;
 use zephyr::raw::ZR_GPIO_OUTPUT_ACTIVE;
 use zephyr::time::{sleep, Duration};
 
-use driver::rht_sensor::read_sensor_example;
+use crate::rht_sensor::{RhtSensor, sht3x::Sht3x, onewire::OneWire};
 
 #[unsafe(no_mangle)]
 extern "C" fn rust_main() {
@@ -39,18 +39,31 @@ fn do_blink() {
         loop {}
     }
 
-    read_sensor_example();
-
-    // Wrong approach - device tree sensors are not directly supported
-    // - Go through zephyr::raw instead, find the function to get a handle to the sht3xd sensor (might need to write a c-version first!)
-    // if !device_is_ready(zephyr::devicetree::labels::sht3xd::get_instance()) {
-    //     info!("Sensor is not ready");
-    // }
-
     led0.configure(ZR_GPIO_OUTPUT_ACTIVE);
-    let duration = Duration::millis_at_least(100);
+    let duration = Duration::millis_at_least(1000);
+
+    let mut s1 = Sht3x { dev: core::ptr::null() };
+    let mut s2 = OneWire { dev: core::ptr::null() };
+
+    // A "List" of different sensors (Trait Objects)
+    // This is the equivalent of a List<IRhtSensor> in C#
+    let sensors: &mut [&mut dyn RhtSensor] = &mut [&mut s1, &mut s2];    
+
+    for s in &mut *sensors {
+        s.init().unwrap();
+    }
+
     loop {
         led0.toggle_pin();
+
+        for s in &mut *sensors {
+            let (temp, hum) = s.read_data().unwrap();
+            //info!("s: {}:  Temperature: {} C, Humidity: {} %%", s, temp, hum);
+            info!(" - - - - ");
+            info!("Temperature: {} C", temp);
+            info!("Humidity: {} %%", hum);
+        }
+
         sleep(duration);
     }
 }
