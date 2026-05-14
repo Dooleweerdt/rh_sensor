3169b5d48a713b07a404f03a9947dce8e5804ab4
Brian Rasmussen <brian.rasmussen80@gmail.com>
2026-05-14 22:36:05 +0200

    Fixed bug in sensor init c-functions. Added Example Sensor config to the project

--- app/c_src/rht_sensor_init.c
+++ app/c_src/rht_sensor_init.c
@@ -5,21 +5,21 @@ LOG_MODULE_REGISTER(sensor_init, CONFIG_APP_LOG_LEVEL);
 
 const struct device *get_sht3x_device(void) {
     const struct device *dev = DEVICE_DT_GET_ANY(sht3xd);
-	if (dev && !device_is_ready(dev)) return dev;
+	if (dev && device_is_ready(dev)) return dev;
     LOG_ERR("SHT3x sensor is not ready");
     return NULL;
 }
 
 const struct device *get_ds18b20_device(void) {
     const struct device *dev = DEVICE_DT_GET_ANY(ds18b20);
-    if (dev && !device_is_ready(dev)) return dev;
+    if (dev && device_is_ready(dev)) return dev;
     LOG_ERR("DS18B20 sensor is not ready");
     return NULL;
 }
 
 const struct device *get_demo_sensor_device(void) {
-    const struct device *dev = DEVICE_DT_GET_ANY(example_sensor);
-    if (dev && !device_is_ready(dev)) return dev;
+    const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
+    if (dev && device_is_ready(dev)) return dev;
     LOG_ERR("Example (mock) sensor is not ready");
     return NULL;
 }
