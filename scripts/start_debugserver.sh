07fb040fc400cae3705d26348b2ba755ef152298
Brian Rasmussen <brian.rasmussen80@gmail.com>
2026-05-25 09:25:08 +0200

    Fixed debugging configuration for ESP32C3 with HW breakpoints and correct AppImage offset

--- scripts/start_debugserver.sh
+++ scripts/start_debugserver.sh
@@ -4,4 +4,17 @@
 # - "-exec load" to load the program symbols (again?).
 # - "-exec si" to single step once...
 
-/home/brian/Tools/openocd-esp32/src/openocd -c "set ESP_RTOS none" -c "set ESP_FLASH_SIZE 0" -s /home/brian/Projects/zephyr_workspace/zephyr/boards/espressif/esp32c3_devkitm/support -s /home/brian/Tools/openocd-esp32/tcl -f /home/brian/Tools/openocd-esp32/tcl/board/esp32c3-builtin.cfg -c "gdb memory_map disable" -c 'tcl port 6333' -c 'telnet port 4444' -c 'gdb port 3333'
+#openocd -c "set ESP_RTOS none" -f board/esp32c3-builtin.cfg -c "gdb memory_map disable" -c 'tcl port 6333' -c 'telnet port 4444' -c 'gdb port 3333'
+
+openocd \
+  -c "set ESP_RTOS Zephyr" \
+  -f board/esp32c3-builtin.cfg \
+  -c "gdb memory_map disable" \
+  -c 'tcl port 6333' \
+  -c 'telnet port 4444' \
+  -c 'gdb port 3333' \
+  -c "init" \
+  -c "halt" \
+  -c "esp appimage_offset 0x20000" \
+#  -c "esp32c3 configure -event gdb-attach { halt; reset halt; }" \
+#  -c "esp32c3 configure -event gdb-detach { reset run; }"
