133edb38974c58370307ca3982c42a17fee8456c
Brian Rasmussen <brian.rasmussen80@gmail.com>
2026-06-14 14:02:52 +0200

    Added check for WIFI_CREDENTIAL define in Wifi bridge

--- app/c_src/wifi_bridge.c
+++ app/c_src/wifi_bridge.c
@@ -2,12 +2,20 @@
 
 #ifdef WIFI_SSID
 void * wifi_bridge_get_ssid(void) {
+    #ifdef WIFI_CREDENTIALS
     return WIFI_SSID;
+    #else
+    return "WifiSSID";
+    #endif
 }
 #endif
 
 #ifdef WIFI_PSK
 void * wifi_bridge_get_psk(void) {
+    #ifdef WIFI_CREDENTIALS
     return WIFI_PSK;
+    #else
+    return "WifiPSK"
+    #endif
 }
 #endif
